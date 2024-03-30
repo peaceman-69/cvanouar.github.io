a2fffa4eba0d8db1c3fe53f99feccbbadbb6361f
Anouar <abdessking01@gmail.com>
2024-03-30 01:38:02 +0100

    fix

--- envoi.php
+++ envoi.php
@@ -5,12 +5,12 @@ if (isset($_POST['submit'])) {
 	$email =$_POST['emailf'];
 	$subject = $_POST['subjectf'];
     $message = $_POST["messagef"];
-	$txt = 'message: '.$_POST['messagef']." \n\n";
+	$txt = 'message: '. $_POST['messagef'] . " \n\n";
 	$mailTo = "abdessking01@gmail.com";
 	$headers = "From: ".$email;
 	$txt = "Vous avez reçus un mail de la part la société:".' '.$name.".\n\n".$email.".\n\n".$subject.".\n\n".$message ;
 
 }
-mail($mailTo,'a Job offer',$name .' '. $email .' '. $txt, $headers);
+mail($mailTo,'a Job offer', $name . ' ' . $email . ' ' . $txt, $headers);
 header("Location: AnouarBenAbdesselem.html?mailsent");
 ?>
